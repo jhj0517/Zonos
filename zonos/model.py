76f447929e202736ef1e01d87c3779745d6dcb5d
jhj0517 <97279763+jhj0517@users.noreply.github.com>
2025-02-12 20:04:50 +0900

    Port mamba to optional

--- zonos/model.py
+++ zonos/model.py
@@ -5,7 +5,7 @@ import safetensors
 import torch
 import torch.nn as nn
 from huggingface_hub import hf_hub_download
-from mamba_ssm.utils.generation import InferenceParams
+# from mamba_ssm.utils.generation import InferenceParams
 from tqdm import tqdm
 
 from zonos.autoencoder import DACAutoencoder
@@ -15,6 +15,26 @@ from zonos.conditioning import PrefixConditioner
 from zonos.config import ZonosConfig
 from zonos.sampling import sample_from_logits
 from zonos.speaker_cloning import SpeakerEmbeddingLDA
+
+
+@dataclass
+class InferenceParams:
+    """Inference parameters that are passed to the main model in order
+    to efficienly calculate and store the context during inference."""
+
+    max_seqlen: int
+    max_batch_size: int
+    seqlen_offset: int = 0
+    batch_size_offset: int = 0
+    key_value_memory_dict: dict = field(default_factory=dict)
+    lengths_per_sample: Optional[Tensor] = None
+
+    def reset(self, max_seqlen, max_batch_size):
+        self.max_seqlen = max_seqlen
+        self.max_batch_size = max_batch_size
+        self.seqlen_offset = 0
+        if self.lengths_per_sample is not None:
+            self.lengths_per_sample.zero_()
 
 
 class Zonos(nn.Module):
